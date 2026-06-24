800ad04491550b0ceb6de7c5700dbe043d29456a
Joshitha24p <joshitha.joshi2012@gmail.com>
2026-06-24 10:37:45 -0400

    Add files via upload

--- Odd Even.py
+++ Odd Even.py
@@ -4,3 +4,9 @@ if number%2==0 :
     print ("The number is an even number")
 else:
     print ("The number is an odd number")
+
+age=int(input("Enter your age"))
+if age<18 :
+    print ("You are not allowed to vote.")
+else:
+    print ("Welcome, you may now vote")
