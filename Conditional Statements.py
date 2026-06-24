a830683a97c9fcf7ef9801465c9c6d4e942db856
Joshitha24p <joshitha.joshi2012@gmail.com>
2026-06-24 10:13:03 -0400

    Add files via upload

---
+++ Conditional Statements.py
@@ -0,0 +1,10 @@
+a=5
+if a > 0:
+    print (a, "is a positive number")
+else:  
+    print (a, "is a negative number")
+b=-4
+if b > 0:
+    print (b, "is a positive number")
+else:
+    print (b, "is a negative number")
